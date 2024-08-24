bbf8d3f54f35e02de8fb32bde5d1450b1da37a80
timataliatov <ttaliatov@gmail.com>
2024-08-24 15:55:32 +0200

    NewEntryModal modified

--- src/components/Modals/NewEntryModal.jsx
+++ src/components/Modals/NewEntryModal.jsx
@@ -1,5 +1,6 @@
+import { Database } from 'react-feather';
 import { addNewEntry } from '../../utils/localStorage';
-import { Heading } from 'lucide-react';
+import { Calendar, Heading, Image, MessageSquareText } from 'lucide-react';
 
 export const NewEntryModal = () => {
     return (
@@ -9,19 +10,35 @@ export const NewEntryModal = () => {
                     {/* if there is a button in form, it will close the modal */}
                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 </form>
-                <h3 className="font-bold text-lg">Add new entry</h3>
-                <p className="py-4">Fill out the data to add a new entry</p>
-                <form>
-                    <label className="input input-bordered flex items-center gap-2">
-                        <Heading />
-                        <input type="text" className="grow" placeholder="Title" />
-                    </label>
-                </form>
+                <div className='flex flex-col gap-4 py-4 px-4'>
+
+                    <h3 className="font-bold text-3xl">Add new entry</h3>
+                    <p className="py-4 mb-8">Fill out the data to add a new entry</p>
+                    <form className='flex flex-col gap-6'>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <Heading />
+                            <input type="text" className="grow" placeholder="Title" />
+                        </label>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <Calendar />
+                            <input type="text" className="grow" placeholder="Date" />
+                        </label>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <MessageSquareText />
+                            <input type="text" className="grow" placeholder="Text" />
+                        </label>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <Image />
+                            <input type="text" className="grow" placeholder="ImgURL" />
+                        </label>
+
+                    </form>
+                </div>
                 <div className="modal-action">
-                <form method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button className="btn" onClick={addNewEntry}>Save</button>
-                </form>
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-outline" onClick={addNewEntry}>Save</button>
+                    </form>
                 </div>
             </div>
             <form className="modal-backdrop" method="dialog">
