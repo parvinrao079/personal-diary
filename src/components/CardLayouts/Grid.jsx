d67f312f18ad416d1b3e95730f1de2e6ab156a52
timataliatov <ttaliatov@gmail.com>
2024-08-31 14:21:11 +0200

    Grid and Timeline fixed and stylized

--- src/components/CardLayouts/Grid.jsx
+++ src/components/CardLayouts/Grid.jsx
@@ -1,35 +1,42 @@
+import { Calendar } from 'react-feather';
 
 const Grid = ({ entries, setDiaryEntry }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 py-6">
       {entries.map((entry, index) => (
-        <div key={index} className="card bg-dark-900 shadow-md p-4 rounded-lg" onClick={() => setDiaryEntry(entry) }>
+        <div
+          key={index}
+          className="card bg-base-300 shadow-xl hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-1"
+          onClick={() => setDiaryEntry(entry)}
+        >
           {entry.imgURL && (
-            <img src={entry.imgURL} alt="Entry" className="rounded-t-lg w-full h-48 object-cover mb-4" />
+            <img
+              src={entry.imgURL}
+              alt="Entry"
+              className="rounded-xl h-48 w-full object-cover"
+            />
           )}
-          <div className="p-4">
-            <h3 className="text-2xl font-bold mb-2 text-white">{entry.title} <span className="text-lg">😊</span></h3>
-            <p className="text-gray-400 mb-4">{entry.date}</p>
-            <p className="mb-4 text-white">{entry.text}</p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              <span className="flex items-center text-gray-300">
-                <i className="far fa-calendar-alt mr-2"></i> {entry.date}
+          <div className="card-body">
+            <h2 className="card-title text-3xl">
+              {entry.title}{' '}
+              <span className="badge badge-secondary h-6 bg-accent ml-10">
+                {entry.mood}
               </span>
-              <span className="flex items-center text-gray-300">
-                <i className="fas fa-map-marker-alt mr-2"></i> {entry.location || 'Unknown Location'}
-              </span>
-              <span className="flex items-center text-gray-300">
-                <i className="fas fa-music mr-2"></i> {entry.music || 'Unknown Music'}
-              </span>
+            </h2>
+            <p className="text-sm line-clamp-3 py-10">{entry.text}</p>
+            <div className="flex flex-wrap gap-2 mt-2">
+              <div className="badge badge-outline rounded-md h-8">
+                <Calendar className="mr-2" size={13} /> {entry.date}
+              </div>
+              {/*               <div className="badge badge-outline rounded-md h-8">
+                <MapPin className="mr-2" size={13} /> {entry.location}
+              </div> */}
+              {/*               <div className="badge badge-outline rounded-md h-8">
+                <Music className="mr-2" size={13} /> {entry.music}
+              </div> */}
             </div>
-            <div className="flex flex-wrap gap-2 mb-4">
-              <span className="badge bg-primary text-white">#nature</span>
-              <span className="badge bg-primary text-white">#reflection</span>
-            </div>
-            <div className="flex items-center justify-between text-gray-300">
-              <span>
-                <i className="far fa-thumbs-up mr-2"></i>23
-              </span>
+
+            {/*             <div className="flex items-center justify-between text-gray-300">
               <div className="flex space-x-2">
                 {entry.tags?.map((tag, i) => (
                   <span key={i} className="badge bg-secondary text-white">
@@ -37,7 +44,7 @@ const Grid = ({ entries, setDiaryEntry }) => {
                   </span>
                 ))}
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       ))}
